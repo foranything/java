42288b0176f5af2555f6e0c6ea270f4c16c393aa
ihlee <leeinhwan7@dgist.ac.kr>
2020-06-20 15:45:41 +0900

    grid

--- src/Program.java
+++ src/Program.java
@@ -1,12 +1,12 @@
 import spring.di.entity.Exam;
 import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
-import spring.di.ui.InlineExamConsole;
+import spring.di.ui.GridExamConsole;
 
 public class Program {
     public static void main(String[] args) {
         Exam exam = new NewlecExam();
-        ExamConsole console = new InlineExamConsole(exam);
+        ExamConsole console = new GridExamConsole(exam);
         console.print();
     }
 }
